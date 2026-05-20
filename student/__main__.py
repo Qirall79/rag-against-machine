bdd793da8b68e5683475b3938ef2a48c28fa930a
qirall79 <belfatmi79@gmail.com>
2026-05-20 04:46:16 +0100

    Add Fire cli tool

---
+++ student/__main__.py
@@ -0,0 +1,55 @@
+import fire
+from pathlib import Path
+from student.index import build_knowledge_base, index, save_metadata
+from student.search_dataset import load_metadata, load_index, load_json_file, get_answer, write_answers
+
+
+class StudentRAGCli:
+    """
+    RAG Pipeline Retrieval Engine CLI managed by Python Fire
+    """
+
+    def index(self, max_chunk_size: int = 2000):
+        """
+        Index the repository codebase and documentation
+        """
+        print(f"Starting ingestion with max_chunk_size: {max_chunk_size}...")
+
+        python_corpus, python_metadata, markdown_corpus, markdown_metadata = build_knowledge_base(
+            max_chunk_size)
+
+        index(python_corpus, markdown_corpus)
+
+        save_metadata(python_metadata, markdown_metadata)
+
+        print(
+            "Ingestion complete! Indices saved under data/processed/ [cite: 263]")
+
+    def search_dataset(self, dataset_path: str, k: int = 10, save_directory: str = "data/output/search_results"):
+        """
+        Process multiple questions from JSON datasets and save coordinate results
+        """
+        print(f"Loading queries from: {dataset_path}")
+
+        metadata = load_metadata()
+        retriever = load_index()
+        queries = load_json_file(dataset_path)
+
+        results = []
+        for query in queries:
+            search_result = get_answer(query, k, retriever, metadata)
+            results.append(search_result)
+
+        output_name = Path(dataset_path).name
+        output_file_path = Path(save_directory) / output_name
+
+        write_answers(str(output_file_path), results, k)
+        print(f"Saved student_search_results to {output_file_path}")
+
+
+def main():
+    fire.Fire(StudentRAGCli)
+
+
+if __name__ == "__main__":
+    main()
---
+++ student/search_dataset.py
@@ -0,0 +1,71 @@
+import re
+import bm25s
+import json
+import argparse
+from pathlib import Path
+from student.models import MinimalSource, UnansweredQuestion, AnsweredQuestion, MinimalSearchResults
+
+
+def load_metadata() -> list[MinimalSource]:
+    project_root = Path(__file__).parent.parent.resolve()
+    output_dir = project_root / "data" / "processed" / "bm25_index"
+    metadata_file_path = output_dir / "metadata.json"
+
+    metadata = []
+    with open(metadata_file_path, 'r', encoding='utf-8') as f:
+        metadata = json.load(f)
+
+    pydantic_sources = [MinimalSource(**item) for item in metadata]
+    return pydantic_sources
+
+
+def load_index():
+    project_root = Path(__file__).parent.parent.resolve()
+    output_dir = project_root / "data" / "processed" / "bm25_index"
+    return bm25s.BM25.load(output_dir)
+
+
+def load_json_file(dataset_path: str) -> list[UnansweredQuestion]:
+    current_dir = Path(__file__).parent.resolve()
+    absolute_dataset_path = current_dir / dataset_path
+
+    print(absolute_dataset_path)
+
+    questions = []
+
+    with open(str(absolute_dataset_path), 'r', encoding='utf-8') as f:
+        questions = json.load(f).get('rag_questions')
+
+    return [UnansweredQuestion(**question) for question in questions]
+
+
+def write_answers(save_file_path: str, results: list[MinimalSearchResults], k: int):
+    current_dir = Path(__file__).parent.resolve()
+    absolute_save_path = current_dir / save_file_path
+
+    absolute_save_path.parent.mkdir(parents=True, exist_ok=True)
+
+    search_results = []
+
+    for result in results:
+        search_results.append({
+            "question_id": result.question_id,
+            "retrieved_sources": [minimal_source.model_dump()
+                                  for minimal_source in result.retrieved_sources]
+        })
+
+    with open(absolute_save_path, 'w', encoding='utf-8') as f:
+        json.dump({"search_results": search_results, "k": k}, f, indent=4)
+
+
+def get_answer(query: UnansweredQuestion, k, retriever, metadata) -> MinimalSearchResults:
+    query_tokens = bm25s.tokenize(query.question)
+
+    documents, scores = retriever.retrieve(query_tokens=query_tokens, k=k)
+
+    sources = []
+    for document in documents[0]:
+        source_model = metadata[document]
+        sources.append(source_model)
+
+    return MinimalSearchResults(question_id=query.question_id, question=query.question, retrieved_sources=sources)
